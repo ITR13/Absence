838ab24fb49bf379baa9e28087f692ed7606631d
ITR <ITR@live.no>
2016-09-30 20:20:33 +0200

    Initial Commit

---
+++ Assets/BasicController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class BasicController : MonoBehaviour {
+	[SerializeField]
+	PlatformerCharacter2D controller;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		float move = Input.GetAxis("Horizontal");
+		bool crouch = Input.GetKey(KeyCode.LeftShift);
+		bool jump = Input.GetKey(KeyCode.Space);
+		controller.Move(move,crouch, jump);
+	}
+}
