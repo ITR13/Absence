17322eb72cea00b07fce255c8e3eaebc610d6f19
ITR <ITR@live.no>
2016-09-30 20:24:52 +0200

    Switched namespace

--- Assets/BasicController.cs
+++ Assets/BasicController.cs
@@ -2,24 +2,26 @@
 using System.Collections;
 using UnityStandardAssets._2D;
 
-public class BasicController : MonoBehaviour {
-	[SerializeField]
-	PlatformerCharacter2D controller;
+namespace Abscence {
+	public class BasicController : MonoBehaviour {
+		[SerializeField]
+		PlatformerCharacter2D controller;
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start () {
 	
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update () {
 	
-	}
+		}
 
-	void FixedUpdate() {
-		float move = Input.GetAxis("Horizontal");
-		bool crouch = Input.GetKey(KeyCode.LeftShift);
-		bool jump = Input.GetKey(KeyCode.Space);
-		controller.Move(move,crouch, jump);
+		void FixedUpdate() {
+			float move = Input.GetAxis("Horizontal");
+			bool crouch = Input.GetKey(KeyCode.LeftShift);
+			bool jump = Input.GetKey(KeyCode.Space);
+			controller.Move(move,crouch, jump);
+		}
 	}
 }
--- Assets/ProceduralController.cs
+++ Assets/ProceduralController.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ProceduralController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+namespace Abscence {
+	public class ProceduralController : MonoBehaviour {
+		// Use this for initialization
+		void Start () {
 	
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
-	
+		// Update is called once per frame
+		void Update () {
+		
+		}
 	}
 }
